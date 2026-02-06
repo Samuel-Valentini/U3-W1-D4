c7bb93b368aa83efedfd73935814090470bba6f5
Samuel-Valentini <samuelvitalia@gmail.com>
2026-02-06 21:26:46 +0100

    Epibooks comments ok

---
+++ src/assets/components/Booklist.jsx
@@ -0,0 +1,44 @@
+import { Row } from "react-bootstrap";
+import SingleBook from "./SingleBook";
+import Form from "react-bootstrap/Form";
+import { Component } from "react";
+
+class BookList extends Component {
+    state = {
+        search: "",
+        genre: "fantasy",
+    };
+
+    render() {
+        const { list } = this.props;
+        const newList = list.filter((book) =>
+            book.title.toLowerCase().includes(this.state.search.toLowerCase()),
+        );
+
+        return (
+            <>
+                <div className="w-75 m-auto">
+                    <Form.Control
+                        size="lg"
+                        type="text"
+                        placeholder="ricerca per titolo"
+                        value={this.state.search}
+                        onChange={(s) => {
+                            this.setState({
+                                ...this.state,
+                                search: s.target.value,
+                            });
+                        }}
+                    />
+                </div>
+                <br />
+                <Row className="h-100 ">
+                    {newList.map((book) => {
+                        return <SingleBook key={book.asin} book={book} />;
+                    })}
+                </Row>
+            </>
+        );
+    }
+}
+export default BookList;
